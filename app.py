cb48e1e676916acf8d61f94099219fd487bd440e
T125010 <t125010@ed.sus.ac.jp>
2026-01-26 11:07:52 +0900

    八列目を分かりやすくするために名前を変更した

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.title('産業と従業員')
-df=pd.read_csv('data.csv')
+df=pd.read_csv('data.csv',encoding='utf-8', skiprows=10)
+df.rename(columns={df.columns[7]:'産業名'},inplace=True)
+
+
